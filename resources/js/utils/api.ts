9cb4ad108fe677ccd6ffde29c71e6a8e5c7b8bc5
João Fernandes <joao.fernandesds26@gmail.com>
2025-02-25 00:05:18 -0300

    updated utils location

--- resources/js/utils/api.ts
+++ resources/js/utils/api.ts
@@ -1,10 +1,3 @@
-import { clsx, type ClassValue } from 'clsx'
-import { twMerge } from 'tailwind-merge'
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
-
 export const httpGet = async <T>(url: string): Promise<T> => {
   const response = await fetch(url, {
     method: 'GET',
