5d6725fcaeacdbdd7215a317d0983f87bec4b78b
João Fernandes <joao.fernandesds26@gmail.com>
2025-02-25 01:25:28 -0300

    added middleware for quiz access

---
+++ app/Http/Middleware/AuthorizeRoomAccess.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\Participant;
+use App\Models\Room;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthorizeRoomAccess
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        $roomId = $request->route('room');
+        $room = Room::findOrFail($roomId);
+
+        if (!$room instanceof Room) {
+            abort(404, 'Sala não encontrada');
+        }
+
+        if (Auth::check()) {
+            $user = Auth::user();
+
+            if ($room->owner_id !== $user->id) abort(401);
+
+            return $next($request);
+        }
+
+        if (session()->has('participant_id')) {
+            $participantId = session('participant_id');
+            $participant = Participant::find($participantId);
+
+            if (!$participant || $participant->room_id !== $room->id) abort(401);
+
+            return $next($request);
+        }
+
+        abort(401);
+    }
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,10 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
+        $middleware->alias([
+            'room.access' => \App\Http\Middleware\AuthorizeRoomAccess::class,
+        ]);
+
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
