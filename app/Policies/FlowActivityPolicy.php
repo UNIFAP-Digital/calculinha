ba3bf20aa67a2273b67f527ab9c7a0b0fd4cbf97
João Fernandes <joao.fernandesds26@gmail.com>
2025-03-01 20:56:19 -0300

    added `create` authorization

--- app/Policies/FlowActivityPolicy.php
+++ app/Policies/FlowActivityPolicy.php
@@ -2,6 +2,8 @@
 
 namespace App\Policies;
 
+use App\Models\Activity;
+use App\Models\Flow;
 use App\Models\FlowActivity;
 use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
@@ -10,13 +12,25 @@ class FlowActivityPolicy
 {
     use HandlesAuthorization;
 
+    public function create(User $user, Flow $flow, Activity $activity): bool
+    {
+        if ($flow->owner_id === null && $activity->owner_id === null) {
+            return true;
+        }
+
+        $flowOwnedByUser = $flow->owner_id === null || $flow->owner_id === $user->id;
+        $activityOwnedByUser = $activity->owner_id === null || $activity->owner_id === $user->id;
+
+        return $flowOwnedByUser && $activityOwnedByUser;
+    }
+
     public function update(User $user, FlowActivity $flowActivity): bool
     {
-        return $flowActivity->flow->room->owner_id === $user->id;
+        return $flowActivity->flow->owner_id === $user->id;
     }
 
     public function delete(User $user, FlowActivity $flowActivity): bool
     {
-        return $flowActivity->flow->room->owner_id === $user->id;
+        return $flowActivity->flow->owner_id === $user->id;
     }
 }
