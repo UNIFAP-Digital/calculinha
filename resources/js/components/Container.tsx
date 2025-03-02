085150facedd17beae55dfc3123f55140973dfa7
João Fernandes <joao.fernandesds26@gmail.com>
2025-03-02 17:36:32 -0300

    added support to header on `Container` component

--- resources/js/components/Container.tsx
+++ resources/js/components/Container.tsx
@@ -1,15 +1,36 @@
 import { cn } from '@/utils/ui'
-import { PropsWithChildren } from 'react'
+import { PropsWithChildren, ReactNode } from 'react'
 
 type ContainerProps = PropsWithChildren & {
   compact?: boolean
   className?: string
+  header?: ReactNode
 }
 
-export default function Container({ children, className, compact }: ContainerProps) {
+export default function Container({ children, className, compact, header }: ContainerProps) {
   return (
-    <div className={cn('mx-auto w-full max-w-7xl px-4 sm:px-6 lg:px-8', className)}>
-      {!compact ? children : <div className="mx-auto max-w-3xl">{children}</div>}
+    <div className={cn('mx-auto w-full max-w-7xl px-4 sm:px-6 sm:py-6 md:py-8 lg:px-8', className)}>
+      {!compact ? (
+        <>
+          {header && (
+            <div className="pb-8">
+              <div className="flex flex-col gap-4 md:flex-row md:items-center md:justify-between">{header}</div>
+            </div>
+          )}
+          {children}
+        </>
+      ) : (
+        <div className="mx-auto max-w-3xl">
+          <>
+            {header && (
+              <div className="pb-8">
+                <div className="flex flex-col gap-4 md:flex-row md:items-center md:justify-between">{header}</div>
+              </div>
+            )}
+            {children}
+          </>
+        </div>
+      )}
     </div>
   )
 }
